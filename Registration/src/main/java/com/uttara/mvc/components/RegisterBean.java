1f35193a4a6aa3c64896d1d3443d96ca8d65e0da
ASWIN S <aswins2193@gmail.com>
2020-03-03 10:17:20 +0530

    Adding table entity and corresponding bean

---
+++ Registration/src/main/java/com/uttara/mvc/components/RegisterBean.java
@@ -0,0 +1,88 @@
+package com.uttara.mvc.components;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class RegisterBean {
+
+	@NotEmpty
+	@Size(min = 5)
+	private String name;
+	
+	@NotEmpty
+	@Email
+	private String email;
+	
+	@NotEmpty
+	@Size(min = 6)
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RegisterBean other = (RegisterBean) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "RegisterBean [name=" + name + ", email=" + email + ", password=" + password + "]";
+	}
+	
+	
+}
